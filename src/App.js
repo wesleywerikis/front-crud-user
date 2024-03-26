d40fa3b81e1415edee483c1c1a1c5d03ed129335
Wesley Werikis <wesleywerikis@outlook.com>
2024-03-26 17:42:02 -0300

    refactor[app]: replace default component with UserList on main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import UserList from './pages/UserList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-
-      </header>
-    </div>
+    <body>
+      <div className='app-container'>
+        <div className='cont-title'>
+          <h2 className='title-app-gerenc'>Aplicação de Gerenciamento de Usuários</h2>
+        </div>
+          <UserList />
+      </div>
+    </body>
   );
 }
 
 export default App;
+
+
+//    <div className="App">
+//      <header className="App-header">
+//      </header>
+//    </div>
