b566efa7421f1c4f150e072711aadfd6dc88c315
Wesley Werikis <wesleywerikis@outlook.com>
2024-03-27 17:47:46 -0300

    refactor[user-page]: adjusted the structure and remove the component 'div button'

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../styles/UserList.css'
+import '../styles/UserList.css';
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
@@ -20,46 +20,37 @@ const UserList = () => {
 
     return (
         <body>
-            <div className="table-responsive">
-                <div className="table-wrapper">
-                    <div className="table-title">
-                        <div className="">
-                            <div className="col-sm-6">
-                                <h2>Lista de <b>Usuários</b></h2>
-                            </div>
-                            <div className="col-sm-6">
-                                <div className="button-container">
-                                    <a href="#addEmployeeModal" className="btn btn-success" data-toggle="modal">
-                                        <i className="mdi mdi-account-plus"></i> <span>Novo Usuário</span>
-                                    </a>
-                                    <a href="#deleteEmployeeModal" className="btn btn-danger" data-toggle="modal">
-                                        <i className="mdi mdi-delete"></i> <span>Deletar</span>
-                                    </a>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="table-responsive">
+                            <div className="table-wrapper">
+                                <div className="table-title">
+                                    <h2>Lista de <b>Usuários</b></h2>
                                 </div>
+                                <table className='user-table'>
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nome</th>
+                                            <th>Username</th>
+                                            <th>Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {users.map(user => (
+                                            <tr key={user.id}>
+                                                <td>{user.id}</td>
+                                                <td>{user.name}</td>
+                                                <td>{user.username}</td>
+                                                <td>{user.email}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
-
-                    <table className='user-table'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nome</th>
-                                <th>Username</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {users.map(user => (
-                                <tr key={user.id}>
-                                    <td>{user.id}</td>
-                                    <td>{user.name}</td>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
                 </div>
             </div>
         </body>
